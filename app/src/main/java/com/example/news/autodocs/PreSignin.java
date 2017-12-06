649be757371eb9836ce489576067356ff536328c
alienschool <33169103+alienschool@users.noreply.github.com>
2017-12-07 01:48:11 +0500

    Signin, Signup, PreSignin pages and SessionManager class. All changes in this commit are not connected to the system e.g not working just put its code

---
+++ app/src/main/java/com/example/news/autodocs/PreSignin.java
@@ -0,0 +1,13 @@
+package com.example.news.autodocs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PreSignin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pre_signin);
+    }
+}
