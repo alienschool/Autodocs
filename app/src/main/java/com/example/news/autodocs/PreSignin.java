9bf55c0df4ecbc216b90b05546317ac05658e3bd
alienschool <33169103+alienschool@users.noreply.github.com>
2017-12-09 01:45:58 +0500

    Signin, Signup, PreSignin pages and SessionManager class. All changes in this commit are not connected to the system e.g not working just put its code

--- app/src/main/java/com/example/news/autodocs/PreSignin.java
+++ app/src/main/java/com/example/news/autodocs/PreSignin.java
@@ -1,7 +1,10 @@
 package com.example.news.autodocs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PreSignin extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class PreSignin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pre_signin);
+        Button mechanic=(Button)findViewById(R.id.buttonAsMechanic);
+        Button user=(Button)findViewById(R.id.buttonAsUser);
+        mechanic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(PreSignin.this,MechanicActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+        user.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(PreSignin.this,MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
