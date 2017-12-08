9fd9740b0038cea2e1d356638dcb3b8349fe8736
Alpha <Alpha@github.com>
2017-12-09 04:15:12 +0500

    UI change

--- app/src/main/java/com/example/news/autodocs/MechanicActivity.java
+++ app/src/main/java/com/example/news/autodocs/MechanicActivity.java
@@ -6,12 +6,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.ServiceConnection;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 import retrofit2.Call;
--- app/src/main/java/com/example/news/autodocs/PreSignin.java
+++ app/src/main/java/com/example/news/autodocs/PreSignin.java
@@ -1,8 +1,12 @@
 package com.example.news.autodocs;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
@@ -28,6 +32,32 @@ public class PreSignin extends AppCompatActivity {
                 Intent i=new Intent(PreSignin.this,MainActivity.class);
                 startActivity(i);
                 finish();
+            }
+        });
+
+        Button b1=(Button)findViewById(R.id.button);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String tittle="Notisfication";
+                String subject="Its me";
+                String body="No it is me";
+
+                NotificationManager notif=(NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+                Notification notify=new Notification.Builder
+                        (getApplicationContext()).setContentTitle(tittle).setContentText(body).
+                        setContentTitle(subject).setSmallIcon(R.drawable.common_google_signin_btn_icon_dark).build();
+
+                Intent notificationIntent = new Intent(getApplicationContext(), MainActivity.class);
+
+                notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
+                        | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+                PendingIntent intent = PendingIntent.getActivity(getApplicationContext(), 0,
+                        notificationIntent, 0);
+                notify.flags |= Notification.FLAG_AUTO_CANCEL;
+                notif.notify(1, notify);
             }
         });
     }
