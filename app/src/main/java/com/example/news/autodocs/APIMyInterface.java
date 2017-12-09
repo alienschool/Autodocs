3449d852682ba170de7c78cfa033ff1ab52b343a
alienschool <33169103+alienschool@users.noreply.github.com>
2017-12-09 22:03:52 +0500

    Signin, Signup, PreSignin pages and SessionManager class. All changes in this commit are not connected to the system e.g not working just put its code

--- app/src/main/java/com/example/news/autodocs/APIMyInterface.java
+++ app/src/main/java/com/example/news/autodocs/APIMyInterface.java
@@ -14,12 +14,12 @@ public interface APIMyInterface {
 
     @FormUrlEncoded
     @POST("signup.php")
-    Call<User> SignUpMechanic(@Field("name") String name,@Field("phone") String phone,@Field("address") String address,
+    Call<User> SignUpUser(@Field("name") String name,@Field("phone") String phone,@Field("address") String address,
                      @Field("billingAddress") String billingAddress,@Field("membership") String membership,
                      @Field("email") String email, @Field("password") String password);
     @FormUrlEncoded
     @POST("signup.php")
-    Call<User> SignUpUser(@Field("name") String name,@Field("phone") String phone,
+    Call<User> SignUpMechanic(@Field("name") String name,@Field("phone") String phone,
                           @Field("lat") String lat,@Field("lng") String lng,
                           @Field("email") String email, @Field("password") String password);
 
--- app/src/main/java/com/example/news/autodocs/Signup.java
+++ app/src/main/java/com/example/news/autodocs/Signup.java
@@ -1,11 +1,15 @@
 package com.example.news.autodocs;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import retrofit2.Call;
@@ -13,12 +17,18 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class Signup extends AppCompatActivity {
+
+    private RadioGroup mRadioGroup;
+    private RadioButton radioButton;
+    String userType="";
     Button signup;
+    Context mContext;
     EditText name,Emai,Address,BilingAdddress,Password,contact;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        mContext=Signup.this;
         signup=(Button)findViewById(R.id.sbmt);
         name=(EditText)findViewById(R.id.name);
         Emai=(EditText)findViewById(R.id.email);
@@ -26,6 +36,21 @@ public class Signup extends AppCompatActivity {
         BilingAdddress=(EditText)findViewById(R.id.BAddress);
         Password=(EditText)findViewById(R.id.password);
         contact=(EditText)findViewById(R.id.Cntctnumber);
+        mRadioGroup = (RadioGroup) findViewById(R.id.signup_radio);
+        userType= "user";
+        mRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, @IdRes int i) {
+
+                if (i == R.id.signup_radioButtonUser) {
+                    radioButton = (RadioButton) findViewById(R.id.signup_radioButtonUser);
+                } else if (i == R.id.signup_radioButtonMechanic) {
+                    radioButton = (RadioButton) findViewById(R.id.signup_radioButtonMechanic);
+                }
+                userType=radioButton.getTag().toString();
+                Toast.makeText(mContext,radioButton.getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -33,33 +58,39 @@ public class Signup extends AppCompatActivity {
                         ||BilingAdddress.getText().toString().equals("")||Address.getText().toString().equals("")))
                 {
                     APIMyInterface apiMyInterface=APIClient.getApiClient().create(APIMyInterface.class);
-                    Call<User> call=apiMyInterface.Login(name.getText().toString(),contact.getText().toString(),
-                            Address.getText().toString(),BilingAdddress.getText().toString(),"null",Emai.getText().toString(),
+                    Call<User> call=apiMyInterface.SignUpUser(name.getText().toString(),contact.getText().toString(),
+                            Address.getText().toString(),BilingAdddress.getText().toString(),"monthly",Emai.getText().toString(),
                             Password.getText().toString());
                     call.enqueue(new Callback<User>() {
                         @Override
                         public void onResponse(Call<User> call, Response<User> response) {
                             String res = response.body().response;
-                            if (res .equals("Sucess")) {
-                                Intent intnt=new Intent(Signup.this,MainActivity.class);
+                            if (res.equalsIgnoreCase("success")) {
+                                Intent intnt=new Intent(Signup.this,Signin.class);
                                 startActivity(intnt);
                                 finish();
                             }
+                            else if(res.equalsIgnoreCase("already registered")){
+                                Emai.setError("Email already registered");
+                            }
+                            else if(res.equalsIgnoreCase("fail")){
+                                Toast.makeText(mContext, "fail", Toast.LENGTH_SHORT).show();
+                            }
                             else
                             {
-                                Toast.makeText(Signup.this, "Complete all the fields first", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(mContext, res, Toast.LENGTH_SHORT).show();
                             }
                         }
 
                         @Override
                         public void onFailure(Call<User> call, Throwable t) {
-                            Toast.makeText(Signup.this, "Exception: "+t.toString(), Toast.LENGTH_SHORT).show();
+                            Toast.makeText(mContext, "Exception: "+t.toString(), Toast.LENGTH_SHORT).show();
                         }
                     });
 
                 }
                 else {
-                    Toast.makeText(Signup.this, "Complete all the field first", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(mContext, "Complete all the field first", Toast.LENGTH_SHORT).show();
                 }
             }
         });
