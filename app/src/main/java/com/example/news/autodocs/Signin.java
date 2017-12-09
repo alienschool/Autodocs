c280a6ed6281fd352b59a43ac722a14e594d0522
Alpha <Alpha@github.com>
2017-12-09 17:15:53 +0500

    Sgn in and Sign Up

--- app/src/main/java/com/example/news/autodocs/Signin.java
+++ app/src/main/java/com/example/news/autodocs/Signin.java
@@ -1,13 +1,101 @@
 package com.example.news.autodocs;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class Signin extends AppCompatActivity {
-
+Button signin;
+    EditText usernaem,Password;
+    TextView signup;
+    SharedPreferences ss;
+    SharedPreferences.Editor editor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+        ss=getSharedPreferences("LoginData", Context.MODE_PRIVATE);
+        editor=ss.edit();
+        signin=(Button)findViewById(R.id.login_login_button);
+        usernaem=(EditText) findViewById(R.id.login_username_editText);
+        signup=(TextView) findViewById(R.id.login_register_textView);
+        Password=(EditText)findViewById(R.id.login_password_editText);
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!(usernaem.getText().toString().equals("")||Password.getText().toString().equals("")))
+                {
+                    APIMyInterface apiMyInterface=APIClient.getApiClient().create(APIMyInterface.class);
+                    Call<User>call=apiMyInterface.Login(usernaem.getText().toString(),Password.getText().toString());
+                    call.enqueue(new Callback<User>() {
+                        @Override
+                        public void onResponse(Call<User> call, Response<User> response) {
+                            String res = response.body().response;
+                            if (res .equals("Sucess")) {
+                                AlertDialog alertDialog = new AlertDialog.Builder(Signin.this).create();
+                                alertDialog.setTitle("Log in");
+                                alertDialog.setMessage("Remember this Account? ");
+                                alertDialog.setButton(android.app.AlertDialog.BUTTON_POSITIVE, "Yes",
+                                        new DialogInterface.OnClickListener() {
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                editor.putString("Login", "User");
+                                                editor.commit();
+                                                dialog.dismiss();
+                                                Intent intent = new Intent(Signin.this, MainActivity.class);
+                                                startActivity(intent);
+
+
+                                            }
+                                        });
+                                alertDialog.setButton(android.app.AlertDialog.BUTTON_NEGATIVE, "No",
+                                        new DialogInterface.OnClickListener() {
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                dialog.dismiss();
+                                                Intent intent = new Intent(Signin.this, MainActivity.class);
+                                                startActivity(intent);
+
+
+                                            }
+
+                                        });
+
+                                alertDialog.show();
+                            }
+                            else
+                            {
+                                Toast.makeText(Signin.this, "Invalid Username or Password", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<User> call, Throwable t) {
+
+                        }
+                    });
+                }
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Signin.this,Signup.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/news/autodocs/Signup.java
+++ app/src/main/java/com/example/news/autodocs/Signup.java
@@ -1,13 +1,68 @@
 package com.example.news.autodocs;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class Signup extends AppCompatActivity {
-
+    Button signup;
+    EditText name,Emai,Address,BilingAdddress,Password,contact;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        signup=(Button)findViewById(R.id.sbmt);
+        name=(EditText)findViewById(R.id.name);
+        Emai=(EditText)findViewById(R.id.email);
+        Address=(EditText)findViewById(R.id.Address);
+        BilingAdddress=(EditText)findViewById(R.id.BAddress);
+        Password=(EditText)findViewById(R.id.password);
+        contact=(EditText)findViewById(R.id.Cntctnumber);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!(name.getText().toString().equals("")||Password.getText().toString().equals("")||Emai.getText().toString().equals("")
+                        ||BilingAdddress.getText().toString().equals("")||Address.getText().toString().equals("")))
+                {
+                    APIMyInterface apiMyInterface=APIClient.getApiClient().create(APIMyInterface.class);
+                    Call<User> call=apiMyInterface.Login(name.getText().toString(),contact.getText().toString(),
+                            Address.getText().toString(),BilingAdddress.getText().toString(),"null",Emai.getText().toString(),
+                            Password.getText().toString());
+                    call.enqueue(new Callback<User>() {
+                        @Override
+                        public void onResponse(Call<User> call, Response<User> response) {
+                            String res = response.body().response;
+                            if (res .equals("Sucess")) {
+                                Intent intnt=new Intent(Signup.this,MainActivity.class);
+                                startActivity(intnt);
+                                finish();
+                            }
+                            else
+                            {
+                                Toast.makeText(Signup.this, "Complete all the fields first", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<User> call, Throwable t) {
+                            Toast.makeText(Signup.this, "Exception: "+t.toString(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+                }
+                else {
+                    Toast.makeText(Signup.this, "Complete all the field first", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
